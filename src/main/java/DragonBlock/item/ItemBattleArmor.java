1e838c69fe0ac55249e6e8120612e77b2468ed17
hagaaa <aaronp1931@gmail.com>
2021-04-03 11:53:59 -0700

    Has ramen and armor. (armor textures aren't working)

--- src/main/java/DragonBlock/item/ItemBattleArmor.java
+++ src/main/java/DragonBlock/item/ItemBattleArmor.java
@@ -9,13 +9,14 @@ public class ItemBattleArmor extends ItemArmor {
     public ItemBattleArmor(ArmorMaterial p_i45325_1_, int p_i45325_2_, int p_i45325_3_) {
         super(p_i45325_1_, p_i45325_2_, p_i45325_3_);
     }
+
     @Override
     public String getArmorTexture(ItemStack stack, Entity entity, int slot, String type)
     {
         if(this.armorType == 2)
         {
-            return "dbaf:textures/models/armor/black_battle_armor_layer_2.png";
+            return "dbaf:textures/models/armor/cheeselayer2.png";
         }
-        return "dbaf:textures/models/armor/black_battle_armor_layer_1.png";
+        return "dbaf:textures/models/armor/cheeselayer1.png";
     }
 }
